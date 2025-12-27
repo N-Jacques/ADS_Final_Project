4a5e873a5ba8180479b882f87bab75074a4551d1
Niccolo <njroxas613@gmail.com>
2025-12-27 15:57:55 +0800

    code improvement

--- index.php
+++ index.php
@@ -5,15 +5,6 @@
 
 session_start();
 
-// 1. Check if the user is already logged in
-// If yes, send them straight to their profile
-if (isset($_SESSION['user_role']) && $_SESSION['user_role'] === 'student') {
-    header("Location: frontend/studentProfile.html");
-    exit();
-}
-
-// 2. If not logged in, redirect immediately to the Login Page
-// Note: Based on your directory listing, ensure the casing matches your file 'login_wPass.html'
 header("Location: frontend/login_wPass.html");
 exit();
 ?>
